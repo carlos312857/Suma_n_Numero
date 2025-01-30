9f5f495e9753a5c0038f5c79d57771e2f5494ff2
carlos312857 <carlosgalvis@colegioguanenta.edu.co>
2025-01-30 17:25:46 -0500

    se agrego el codigo

---
+++ suma_n_numeros.py
@@ -0,0 +1,14 @@
+# imput 
+print("#######################")
+n= input("dijite su numero")
+print("#######################")
+n=int(n)
+
+
+# formula
+s = (n * (n+1))/2
+
+#output
+print("#######################")
+print("la suma de los numeros ",s)
+print("########################")
